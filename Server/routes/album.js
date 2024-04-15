fbf30e43e3a7ecef1c0c33b60156072ec99c49c8
rishueee <jhapinki567@gmail.com>
2024-04-15 11:06:13 +0530

    Put Method Added

--- Server/routes/album.js
+++ Server/routes/album.js
@@ -19,4 +19,23 @@ router.post("/save", async (req, res) => {
     }
     })
 
+
+    router.put("/update/:id", async(req, res) => {
+        const filter = {_id:req.params.id};
+       
+       
+        try{
+            const result = await album.findOneAndUpdate(filter, { 
+                name: req.body.name,
+                imageurl: req.body.imageurl,
+               
+            }
+            );
+            res.status(200).send({album:result})
+        }
+        catch(error){
+            res.status(400).send({sucess:false, message:error})
+        }
+    })
+
     module.exports = router
